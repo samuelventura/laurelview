ecec6fa974afa32a5dbb8be5505cb94c1e4a0a19
Samuel Ventura <samuel.ventura@yeico.com>
2021-08-17 13:32:45 -0500

    msys detection

--- install.sh
+++ install.sh
@@ -7,11 +7,13 @@ mkdir -p pkg/lvnbe/build
 touch pkg/lvnbe/build/.empty
 cp -fr web/lvnfe/build pkg/lvnbe/
 MOD="github.com/samuelventura/laurelview"
-#go get github.com/akavel/rsrc
-rsrc -ico icon.ico -o build/rsrc.syso
-cp build/rsrc.syso cmd/lvnss/
-cp build/rsrc.syso cmd/lvnbe/
-cp build/rsrc.syso cmd/lvnrt/
+if [[ "$OSTYPE" == "msys"* ]]; then
+    #go get github.com/akavel/rsrc
+    rsrc -ico icon.ico -o build/rsrc.syso
+    cp build/rsrc.syso cmd/lvnss/
+    cp build/rsrc.syso cmd/lvnbe/
+    cp build/rsrc.syso cmd/lvnrt/
+fi
 go install $MOD/cmd/lvdpm
 go install $MOD/cmd/lvnbe
 go install $MOD/cmd/lvnrt
