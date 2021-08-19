558868e88150a49a75c6f5036b118f879e76da19
Samuel Ventura <samuel.ventura@yeico.com>
2021-08-18 21:05:59 -0500

    /usr/local for linux se

--- install.sh
+++ install.sh
@@ -1,20 +1,10 @@
 #!/bin/sh -x
 
-[ ! -d "web/lvnfe/node_modules" ] && (cd web/lvnfe; npm i)
-(cd web/lvnfe; npm run build)
-rm -fr pkg/lvnbe/build
-mkdir -p pkg/lvnbe/build
-touch pkg/lvnbe/build/.empty
-cp -fr web/lvnfe/build pkg/lvnbe/
-MOD="github.com/samuelventura/laurelview"
-if [[ "$OSTYPE" == "msys"* ]]; then
-    #go get github.com/akavel/rsrc
-    rsrc -ico icon.ico -o build/rsrc.syso
-    cp build/rsrc.syso cmd/lvnss/
-    cp build/rsrc.syso cmd/lvnbe/
-    cp build/rsrc.syso cmd/lvnrt/
+if [[ "$OSTYPE" == "linux"* ]]; then
+    SRC=$HOME/go/bin
+    DST=/usr/local/bin
+    sudo cp $SRC/lvnbe $DST
+    sudo cp $SRC/lvnrt $DST
+    sudo cp $SRC/lvnss $DST
+    sudo $DST/lvnss -service install
 fi
-go install $MOD/cmd/lvdpm
-go install $MOD/cmd/lvnbe
-go install $MOD/cmd/lvnrt
-go install $MOD/cmd/lvnss
