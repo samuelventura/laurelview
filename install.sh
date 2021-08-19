eea661138760922096317aac4f3b734945533cb9
Samuel Ventura <samuel.ventura@yeico.com>
2021-08-19 01:12:50 -0500

    linux install

--- install.sh
+++ install.sh
@@ -3,8 +3,13 @@
 if [[ "$OSTYPE" == "linux"* ]]; then
     SRC=$HOME/go/bin
     DST=/usr/local/bin
+    if [[ -f "$DST/lvnss" ]]; then
+        sudo systemctl stop LaurelView
+        sudo $DST/lvnss -service uninstall
+    fi
     sudo cp $SRC/lvnbe $DST
     sudo cp $SRC/lvnrt $DST
     sudo cp $SRC/lvnss $DST
     sudo $DST/lvnss -service install
+    sudo systemctl restart LaurelView
 fi
