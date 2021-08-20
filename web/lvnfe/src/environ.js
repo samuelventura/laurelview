0d47c20dc9a5a05ce8c4df4503d206a0e3e9b1aa
Samuel Ventura <samuel.ventura@yeico.com>
2021-08-20 00:42:09 -0500

    public ws urls

--- web/lvnfe/src/environ.js
+++ web/lvnfe/src/environ.js
@@ -3,8 +3,13 @@ const devWcURL = "ws://localhost:5001/ws"
 const devRtURL = "ws://localhost:5002/ws"
 const location = window.location
 const port = parseInt(location.port)
-const prodWcURL = "ws://" + location.host + "/ws"
-const prodRtURL = "ws://" + location.hostname + ":" + (port+1) + "/ws"
+let prodWcURL = "ws://" + location.host + "/ws"
+let prodRtURL = "ws://" + location.hostname + ":" + (port+1) + "/ws"
+
+if (location.hostname==="laurelview.io") {
+    prodWcURL = "wss://laurelview.io/ws"
+    prodRtURL = "wss://wss.laurelview.io/ws"
+}
 
 const isDev = process.env.NODE_ENV === 'development'
 
