22a46ba607506e83c6dbdcbe67d65078cffd098e
Samuel Ventura <samuel.ventura@yeico.com>
2021-08-20 12:09:05 -0500

    drop detection 9s

--- pkg/lvnrt/bus.go
+++ pkg/lvnrt/bus.go
@@ -180,8 +180,15 @@ func NewBus(rt Runtime) Dispatch {
 			for {
 				conn, err := net.DialTimeout("tcp", address, Millis(dialtoms))
 				if err == nil {
+					defer conn.Close()
+					//connection drop detection macos=~9s
 					//set data asap, do not wait for larger packet
-					err = conn.(*net.TCPConn).SetNoDelay(true)
+					tcp := conn.(*net.TCPConn)
+					tcp.SetNoDelay(true)
+					tcp.SetLinger(0)
+					tcp.SetKeepAlive(true)
+					tcp.SetKeepAlivePeriod(Millis(1000))
+					tcp.SetWriteBuffer(0)
 				}
 				TraceIfError(log.Trace, err)
 				if err != nil {
