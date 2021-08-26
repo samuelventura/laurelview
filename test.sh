69604c027f89072b1323c2c6d723939b24bc48d2
Samuel Ventura <samuel.ventura@yeico.com>
2021-08-26 12:18:20 -0500

    test scope and pkg

--- test.sh
+++ test.sh
@@ -1,10 +1,25 @@
 #!/bin/sh -x
 
 #trace|debug|info
-TEST_SCOPE="${1:-Test}"
-export LV_LOGLEVEL="${2:-info}"
+TEST_PKG="${1:-all}"
+TEST_SCOPE="${2:-Test}"
+export LV_LOGLEVEL="${3:-info}"
 MOD="github.com/samuelventura/laurelview"
 go clean -testcache 
-go test $MOD/pkg/lvndb -v -run $TEST_SCOPE
-go test $MOD/pkg/lvnrt -v -run $TEST_SCOPE
-go test $MOD/pkg/lvsdk -v -run $TEST_SCOPE
+
+case $TEST_PKG in
+    all)
+    go test $MOD/pkg/lvsdk -v -run $TEST_SCOPE
+    go test $MOD/pkg/lvndb -v -run $TEST_SCOPE
+    go test $MOD/pkg/lvnrt -v -run $TEST_SCOPE
+    ;;
+    db)
+    go test $MOD/pkg/lvndb -v -run $TEST_SCOPE
+    ;;
+    rt)
+    go test $MOD/pkg/lvnrt -v -run $TEST_SCOPE
+    ;;
+    sdk)
+    go test $MOD/pkg/lvsdk -v -run $TEST_SCOPE
+    ;;
+esac
--- unit_sdk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -x
-
-#trace|debug|info
-TEST_SCOPE="${1:-Test}"
-export LV_LOGLEVEL="${2:-info}"
-MOD="github.com/samuelventura/laurelview"
-go clean -testcache 
-go test $MOD/pkg/lvsdk -v -run $TEST_SCOPE
