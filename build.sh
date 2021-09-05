627c61fc5650e375143bb3298d900f41f7d8aeb9
Samuel Ventura <samuel.ventura@yeico.com>
2021-09-05 14:49:15 -0500

    pi build

--- build.sh
+++ build.sh
@@ -1,19 +1,29 @@
 #!/bin/sh -x
 
-[ ! -d "web/lvnfe/node_modules" ] && (cd web/lvnfe; npm i)
-(cd web/lvnfe; npm run build)
-rm -fr cmd/lvnbe/build
-mkdir -p cmd/lvnbe/build
-touch cmd/lvnbe/build/.empty
-cp -fr web/lvnfe/build cmd/lvnbe/
+export TARGET="${1:-local}"
+
 MOD="github.com/samuelventura/laurelview"
-if [[ "$OSTYPE" == "msys"* ]]; then
-    #go get github.com/akavel/rsrc
-    rsrc -ico icon.ico -o build/rsrc.syso
-    cp build/rsrc.syso cmd/lvnss/
-    cp build/rsrc.syso cmd/lvnbe/
-fi
-go install $MOD/cmd/lvdpm
-go install $MOD/cmd/lvnbe
-go install $MOD/cmd/lvnup
-go install $MOD/cmd/lvnss
+
+case $TARGET in
+    local)
+    if [[ "$OSTYPE" == "msys"* ]]; then
+        #go get github.com/akavel/rsrc
+        rsrc -ico icon.ico -o build/rsrc.syso
+        cp build/rsrc.syso cmd/lvnss/
+        cp build/rsrc.syso cmd/lvnbe/
+    fi
+    go install $MOD/cmd/lvdpm
+    go install $MOD/cmd/lvnbe
+    go install $MOD/cmd/lvnup
+    go install $MOD/cmd/lvnss
+    ;;
+    pi)
+    OUT=/tmp/pibuild
+    mkdir -p $OUT
+    FLAGS="GOOS=linux GOARCH=arm GOARM=7"
+    env $FLAGS go build -o $OUT/lvdpm $MOD/cmd/lvdpm
+    env $FLAGS go build -o $OUT/lvnbe $MOD/cmd/lvnbe
+    env $FLAGS go build -o $OUT/lvnup $MOD/cmd/lvnup
+    env $FLAGS go build -o $OUT/lvnss $MOD/cmd/lvnss
+    ;;
+esac
---
+++ pack.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -x
+
+[ ! -d "web/lvnfe/node_modules" ] && (cd web/lvnfe; npm i)
+(cd web/lvnfe; npm run build)
+rm -fr cmd/lvnbe/build
+mkdir -p cmd/lvnbe/build
+touch cmd/lvnbe/build/.empty
+cp -fr web/lvnfe/build cmd/lvnbe/
