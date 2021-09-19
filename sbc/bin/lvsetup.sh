b0479ac7452be5f103ed550934e80ed68dae9f86
jesusrevilla <jesus_revilla@yahoo.com>
2021-09-19 15:03:13 -0500

    Update lvsetup.sh
    Change lvdb.db3 database path to /mnt/usb1

--- sbc/bin/lvsetup.sh
+++ sbc/bin/lvsetup.sh
@@ -8,5 +8,5 @@ echo "LV_NBE_ENDPOINT=0.0.0.0:80" > /usr/local/bin/lvnss.config
 echo "LV_NUP_ENDPOINT=127.0.0.1:80" >> /usr/local/bin/lvnss.config
 echo "LV_DPM_ENDPOINT=127.0.0.1:81" >> /usr/local/bin/lvnss.config
 echo "LV_NBE_DEBUG=127.0.0.1:82" >> /usr/local/bin/lvnss.config
-echo "LV_NBE_DATABASE=/var/lvdb.db3" >> /usr/local/bin/lvnss.config
+echo "LV_NBE_DATABASE=/mnt/usb1/lvdb.db3" >> /usr/local/bin/lvnss.config
 echo "LV_NSS_LOGS=/tmp" >> /usr/local/bin/lvnss.config
