2ed8de2ac8914c9e9e51b4ea80155d0b6903c6b1
Samuel Ventura <samuel.ventura@yeico.com>
2021-09-25 23:33:52 -0500

    0.0.4 windows build msys2 go-sqlite3 cloud link icon

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ if [[ "$OSTYPE" == "msys"* ]]; then
     rsrc -ico icon.ico -o build/rsrc.syso
     cp build/rsrc.syso cmd/lvnss/
     cp build/rsrc.syso cmd/lvnbe/
+    cp build/rsrc.syso cmd/lvnup/
+    cp build/rsrc.syso cmd/lvdpm/
 fi
 #remove lvtry.exe
 #rm /c/Users/samuel/go/bin/lv*.exe
